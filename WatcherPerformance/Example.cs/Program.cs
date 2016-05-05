861bedb5b811acc3b46650a6eed3016cb0c1a9ea
cuirongqing <cuirongqing@outlook.com>
2016-05-05 17:12:55 +0800

    Add PerformaceWatcher to watch every execution in the single thread. It can dump execution text and its execution time. It can work in MVC, EF, sql, WCF inspector and everywhere you watch to watch.

---
+++ WatcherPerformance/Example.cs/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Dispatcher;
+using System.Threading;
+using System.Web.Mvc;
+
+using PerformcanceWatcherFramework;
+
+namespace Example.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watcher = PerformanceWatcher.StartNewWatcher();
+
+            #region Executions in the single thread.
+
+            PerformanceWatcher.Watch("Test1", () =>
+            {
+                Thread.Sleep(1234);
+            });
+
+            PerformanceWatcher.Watch("Test1", () =>
+             {
+                 Thread.Sleep(5678);
+
+                 return 1;
+             });
+
+            #endregion
+
+            var watchedResult = watcher.StopWatcher();
+            Console.WriteLine(watchedResult);
+
+            Console.ReadLine();
+
+            // Expected Result:
+            // ThreadId: 10, ElapsedTIme: 6912
+            //Test1 1234
+            //Test1 5678
+        }
+    }
+
+    #region MVC & Watches every action execution
+
+    /// <summary>
+    /// Watches every action execution.
+    /// </summary>
+    public class PerformanceWatcherActionFilterAttribute : ActionFilterAttribute, IActionFilter
+    {
+        PerformanceWatcher watcher = null;
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            watcher = PerformanceWatcher.StartNewWatcher();
+
+            base.OnActionExecuting(filterContext);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            if (watcher != null)
+            {
+                var watchedResult = watcher.StopWatcher();
+                if (watchedResult.ElapsedTime >= 3000)
+                {
+                    var controller = filterContext.RouteData.Values["controller"];
+                    var action = filterContext.RouteData.Values["action"];
+                    // logger.Log(Level.Warning, string.Format("Action: {0}/{1}, Results: {2}", controller, action, watchedResult.ToString()));
+                }
+            }
+
+            base.OnActionExecuted(filterContext);
+        }
+    }
+
+    #endregion
+
+    #region SQL & SqlHelper
+
+    //private static SqlDataReader ExecuteReader(SqlConnection connection, SqlTransaction transaction, CommandType commandType, string commandText, SqlParameter[] commandParameters, SqlConnectionOwnership connectionOwnership)
+    //{
+    //    //create a command and prepare it for execution
+    //    SqlCommand cmd = new SqlCommand();
+    //    PrepareCommand(cmd, connection, transaction, commandType, commandText, commandParameters);
+
+    //    //create a reader
+    //    SqlDataReader dr;
+
+    //    // call ExecuteReader with the appropriate CommandBehavior
+    //    if (connectionOwnership == SqlConnectionOwnership.External)
+    //    {
+    //        dr = PerformanceWatcher.Watch(commandText, () => cmd.ExecuteReader());
+    //    }
+    //    else
+    //    {
+    //        dr = PerformanceWatcher.Watch(commandText, () => cmd.ExecuteReader(CommandBehavior.CloseConnection));
+    //    }
+
+    //    // detach the SqlParameters from the command object, so they can be used again.
+    //    cmd.Parameters.Clear();
+
+    //    return dr;
+    //}
+
+    #endregion
+
+    #region WCF Inspector
+
+    public class WatchServerThresholdMessageInspector : IDispatchMessageInspector
+    {
+        #region IDispatchMessageInspector
+
+        private PerformanceWatcher watcher = null;
+
+        public object AfterReceiveRequest(ref Message request, IClientChannel channel, InstanceContext instanceContext)
+        {
+            return watcher = PerformanceWatcher.StartNewWatcher();
+        }
+
+        public void BeforeSendReply(ref Message reply, object correlationState)
+        {
+            var watchedResult = watcher.StopWatcher();
+            if (watchedResult.ElapsedTime >= 1000)
+            {
+                ThreadPool.QueueUserWorkItem(_ =>
+                {
+                    //logger.Log(Level.Warning, string.Format("Action: {0}, Request: {1}, Response: {2}, TotalMilliseconds: {3}",
+                    //                               messageWatcher.Action, messageWatcher.Message,
+                    //                               responseMessage.ToString(), elapsedTime));
+                }, reply.ToString());
+
+                using (var messageBuffer = reply.CreateBufferedCopy(int.MaxValue))
+                {
+                    reply = messageBuffer.CreateMessage();
+                }
+            }
+        }
+
+        #endregion
+    }
+
+    #endregion
+}
---
+++ WatcherPerformance/WatcherFramework/PerformanceWatcher.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+
+namespace PerformcanceWatcherFramework
+{
+    /// <summary>
+    /// Starts a performance watcher to watch every execution in every thread.
+    /// If threshold is exceeded, it can dump every method and its execution time.
+    /// </summary>
+    public class PerformanceWatcher
+    {
+        /// <summary>
+        /// Saves all the execution and its execution time.
+        /// </summary>
+        [ThreadStatic]
+        private static WatchedResult watchedResult;
+
+        [ThreadStatic]
+        private static DateTime? startTime = null;
+
+        private PerformanceWatcher() { }
+
+        /// <summary>
+        /// Initials a new performcane watcher.
+        /// </summary>
+        /// <returns></returns>
+        public static PerformanceWatcher StartNewWatcher()
+        {
+            startTime = DateTime.Now;
+            watchedResult = new WatchedResult();
+
+            return new PerformanceWatcher();
+        }
+
+        /// <summary>
+        /// Stops to watch performance and gets watched result.
+        /// </summary>
+        /// <returns>Watched result.</returns>
+        public WatchedResult StopWatcher()
+        {
+            var results = new WatchedResult();
+            if (startTime.HasValue && watchedResult != null)
+            {
+                results.ElapsedTime = (DateTime.Now - startTime.Value).TotalMilliseconds;
+                results.Results.AddRange(watchedResult.Results);
+            }
+
+            startTime = null;
+            watchedResult = null;
+
+            return results;
+        }
+
+        /// <summary>
+        /// Watches a single method and dumps the method name with its execution time.
+        /// </summary>
+        /// <param name="watchingCall">Execution text such as RPC name, stored proc name and sql script.</param>
+        /// <param name="action"></param>
+        public static void Watch(string watchingCall, Action action)
+        {
+            if (string.IsNullOrEmpty(watchingCall) || action == null || !startTime.HasValue)
+            {
+                action();
+
+                return;
+            }
+
+            var stopWatcher = new Stopwatch();
+            try
+            {
+                stopWatcher.Start();
+
+                action();
+            }
+            finally
+            {
+                stopWatcher.Stop();
+
+                if (watchedResult == null)
+                {
+                    watchedResult = new WatchedResult();
+                }
+
+                // Dumps the method name with its execution time.
+                watchedResult.Results.Add(new WatchedResult.WatchingResult(watchingCall, stopWatcher.ElapsedMilliseconds));
+            }
+        }
+
+        /// <summary>
+        /// Watches a single method and dumps the method name with its execution time.
+        /// </summary>
+        /// <param name="watchingCall">Execution text such as RPC name, stored proc name and sql script.</param>
+        public static TResult Watch<TResult>(string watchingCall, Func<TResult> func)
+        {
+            if (string.IsNullOrEmpty(watchingCall) || func == null || !startTime.HasValue)
+            {
+                return func();
+            }
+
+            var stopWatcher = new Stopwatch();
+            try
+            {
+                stopWatcher.Start();
+
+                return func();
+            }
+            finally
+            {
+                stopWatcher.Stop();
+
+                if (watchedResult == null)
+                {
+                    watchedResult = new WatchedResult();
+                }
+
+                watchedResult.Results.Add(new WatchedResult.WatchingResult(watchingCall, stopWatcher.ElapsedMilliseconds));
+            }
+        }
+
+        /// <summary>
+        /// Directlly dumps the method name and its execution time.
+        /// </summary>
+        public static void Watch(string watchingCall, long elapsedTime)
+        {
+            if (startTime.HasValue && !string.IsNullOrEmpty(watchingCall))
+            {
+                watchedResult.Results.Add(new WatchedResult.WatchingResult(watchingCall, elapsedTime));
+            }
+        }
+    }
+
+    /// <summary>
+    /// Dumps all the execution and its execution time.
+    /// </summary>
+    public class WatchedResult
+    {
+        public List<WatchingResult> Results { get; private set; }
+        public double ElapsedTime { get; set; }
+
+        public WatchedResult()
+        {
+            this.Results = new List<WatchingResult>();
+        }
+
+        public override string ToString()
+        {
+            var watcherResultBuilder = new StringBuilder();
+            watcherResultBuilder.AppendFormat("ThreadId:{0}, ElapsedTIme:{1}", Thread.CurrentThread.ManagedThreadId, this.ElapsedTime);
+
+            this.Results.ForEach(_ =>
+            {
+                watcherResultBuilder.AppendLine().Append(_.ToString());
+            });
+
+
+            return watcherResultBuilder.ToString();
+        }
+
+        /// <summary>
+        /// Defines execution method and its execution time in a single thread.
+        /// </summary>
+        public class WatchingResult
+        {
+            #region Ctor
+
+            public string WatchingCall { get; private set; }
+            public long ElapsedTime { get; private set; }
+
+            public WatchingResult(string watchingCall, long elapsedTime)
+            {
+                this.WatchingCall = watchingCall;
+                this.ElapsedTime = elapsedTime;
+            }
+
+            #endregion
+
+            public override string ToString()
+            {
+                return string.Format("{0} {1}", this.WatchingCall, this.ElapsedTime);
+            }
+        }
+    }
+}
